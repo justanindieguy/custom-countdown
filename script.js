e167af93de7643aa2243921ec7eb0e9d62b2c0c2
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2022-01-25 17:19:57 -0600

    take values from form when submit event is triggered

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdown-form');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Minimum with Today's Date
 const [today] = new Date().toISOString().split('T');
 dateEl.setAttribute('min', today);
+
+// Take Values from Form Input
+const updateCountdown = (evt) => {
+  evt.preventDefault();
+  countdownTitle = evt.srcElement[0].value;
+  countdownDate = evt.srcElement[1].value;
+};
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
